37a28f4a801e6e92e1454d1384a247bb6a8dd6cf
RivkaDF <37163569+RivkaDF@users.noreply.github.com>
2018-04-12 23:02:16 -0500

    Add files via upload

---
+++ gradientFunctionReg.py
@@ -0,0 +1,28 @@
+from numpy import asfortranarray, squeeze, asarray
+import numpy as np
+
+from gradientFunction import gradientFunction
+
+
+def gradientFunctionReg(theta, X, y, Lambda):
+    """
+    Compute cost and gradient for logistic regression with regularization
+
+    computes the cost of using theta as the parameter for regularized logistic regression and the
+    gradient of the cost w.r.t. to the parameters.
+    """
+    m = len(y)   # number of training examples
+
+# ====================== YOUR CODE HERE ======================
+# Instructions: Compute the gradient of a particular choice of theta.
+#               Compute the partial derivatives and set grad to the partial
+#               derivatives of the cost w.r.t. each parameter in theta
+
+
+# =============================================================
+   
+    grad = gradientFunction(theta, X, y) + ((Lambda/m) * theta)
+    
+    #upgrade theta zero without regularization
+    grad[0] = gradientFunction(theta,X,y)[0]
+    return grad
